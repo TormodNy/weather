3251d4eb3b000e4f423feb582d90507293e1899f
TormodNy <tormodn99@gmail.com>
2024-10-31 21:54:18 +0100

    feat: add basic box for selecting weather location

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,9 +1,21 @@
 import BasePage from "../base/BasePage";
+import LocationBox from "./LocationBox";
 
 function DashboardPage() {
     return (
         <BasePage heading="Dashboard">
-            Insert weather here
+            <section className="flex flex-row gap-8 w-full justify-center">
+                <LocationBox
+                    locationName="London"
+                    latitude={51.509865}
+                    longitude={-0.118092}
+                />
+                <LocationBox
+                    locationName="Berlin"
+                    latitude={52.520008}
+                    longitude={13.404954}
+                />
+            </section>
         </BasePage>
     );
 }
---
+++ src/pages/dashboard/LocationBox.tsx
@@ -0,0 +1,16 @@
+interface LocationBoxProps {
+    locationName: string;
+    latitude: number;
+    longitude: number;
+}
+
+function LocationBox({ locationName, latitude, longitude }: LocationBoxProps) {
+    return (
+        <button className="flex justify-between bg-gray-900 p-4 gap-2 text-xl max-w-96 grow">
+            <span>{locationName}</span>
+            <span>15°C</span>
+        </button>
+    );
+}
+
+export default LocationBox;
