1f2d9488ef66d6032ce7e9f7734a5fe0efffac45
TormodNy <tormodn99@gmail.com>
2024-11-02 11:43:37 +0100

    feat: fetch forecast data and display temperatures on dashboard

--- src/pages/dashboard/LocationBox.tsx
+++ src/pages/dashboard/LocationBox.tsx
@@ -1,3 +1,7 @@
+import { useCallback, useEffect, useState } from "react";
+import { LocationForecastService } from "../../clients";
+import { GetCompactResponse } from "../../clients/locationforecast";
+
 interface LocationBoxProps {
     locationName: string;
     latitude: number;
@@ -5,10 +9,31 @@ interface LocationBoxProps {
 }
 
 function LocationBox({ locationName, latitude, longitude }: LocationBoxProps) {
+    const [forecast, setForecast] = useState<GetCompactResponse | null>(null);
+    const temperature =
+        forecast?.properties.timeseries[0]?.data.instant.details
+            ?.air_temperature;
+
+    const fetchLocationForecast = useCallback(async () => {
+        const { data } = await LocationForecastService.getCompact({
+            query: {
+                lat: latitude,
+                lon: longitude,
+            },
+        });
+        if (data) {
+            setForecast(data);
+        }
+    }, [latitude, longitude]);
+
+    useEffect(() => {
+        fetchLocationForecast();
+    }, [fetchLocationForecast]);
+
     return (
         <button className="flex justify-between bg-gray-900 p-4 gap-2 text-xl max-w-96 w-full">
             <span>{locationName}</span>
-            <span>15°C</span>
+            <span>{temperature !== undefined ? `${temperature}°C` : "-"}</span>
         </button>
     );
 }
